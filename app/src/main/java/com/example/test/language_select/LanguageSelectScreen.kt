71fc01f43526f90ef366a64a908807c0470a11a7
alexey <alxgor@ya.ru>
2025-09-25 00:44:52 +0500

    realized sign up screen, validation for email

--- app/src/main/java/com/example/test/language_select/LanguageSelectScreen.kt
+++ app/src/main/java/com/example/test/language_select/LanguageSelectScreen.kt
@@ -1,18 +1,17 @@
 package com.example.test.language_select
 
 
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
-import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.lazy.itemsIndexed
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Button
-import androidx.compose.material3.ButtonColors
 import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
@@ -20,6 +19,7 @@ import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.focus.FocusRequester.Companion.createRefs
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalConfiguration
 import androidx.compose.ui.text.font.Font
@@ -28,6 +28,7 @@ import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import androidx.constraintlayout.compose.ConstraintLayout
 import androidx.constraintlayout.widget.ConstraintLayout
 import androidx.navigation.NavController
 import com.example.test.R
@@ -48,83 +49,116 @@ fun LanguageSelectScreen(
     val height = configuration.screenHeightDp
     val languages = listOf("Russian", "English", "Chinese", "Belarus", "Kazakh")
 
-    ConstraintLayout(
-        modifier = Modifier.padding(
-            top = (height * 90 / 812).dp,
-            start = (width * 24 / 375).dp,
-            end = (width * 24 / 375).dp
-        ).fillMaxSize()
-    ) {
-        Text(
-            "What is your Mother language?",
-            fontFamily = fonts,
-            fontWeight = FontWeight.Medium,
-            fontSize = 22.sp,
-            color = Color(0xFF080E1E),
-            modifier = Modifier
-                .padding(bottom = (height * 16 / 812).dp)
-        )
-        val ind = remember { mutableStateOf("") }
-        LazyColumn(
-            verticalArrangement = Arrangement.spacedBy(
-                (height * 12 / 812).dp,
-                Alignment.CenterVertically
-            )
-        ) {
-            itemsIndexed(languages) { index, lang ->
-                Button(
-                    onClick = {
-                        ind.value = lang
-                    },
-                    modifier = Modifier
-                        .fillMaxWidth(),
-                    colors = ButtonDefaults.buttonColors(
-                        containerColor =
-                        if (ind.value == lang) {
-                            Color(0xFFF76400)
-                        } else {
-                            Color(0xFFFFF6EB)
-                        }
-                    ),
-                    shape = RoundedCornerShape(20.dp)
-                ) {
-                    Text(
-                        lang,
-                        modifier = Modifier
-                            .padding(
-                                top = (height * 20 / 812).dp,
-                                bottom = (height * 20 / 812).dp
-                            )
-                            .fillMaxWidth(),
-                        fontFamily = fonts,
-                        fontWeight = FontWeight.Medium,
-                        fontSize = (height * 22 / 812).sp,
-                        color = Color.Black,
-                        textAlign = TextAlign.Start
-                    )
-                }
-
-            }
-        }
-        Button(
-            onClick = { navController.navigate(Route.SignUp.route) },
-            modifier = Modifier
-                .align(Alignment.CenterHorizontally)
-                .fillMaxWidth(),
-            colors = ButtonDefaults.buttonColors(
-                Color(0xFF5B7BFE)
-            ),
-            shape = RoundedCornerShape(12.dp)
-        ) {
+    Column() {
+        Box(modifier = Modifier.fillMaxWidth().background(Color(0xFF410FA3))) {
             Text(
-                "Next",
+                "Language select",
                 fontFamily = fonts,
                 fontWeight = FontWeight.Medium,
-                fontSize = 20.sp,
+                fontSize = (height * 17 / 812).sp,
                 color = Color.White,
-                modifier = Modifier.padding(vertical = (height * 16 / 812).dp)
+                modifier = Modifier
+                    .padding(
+                        top = (height * 60 / 812).dp,
+                        bottom = (height * 20 / 812).dp
+                    )
+                    .align(Alignment.Center)
             )
-        }
 
+        }
+        ConstraintLayout(
+            modifier = Modifier
+                .padding(
+                    top = (height * 12 / 812).dp,
+                    start = (width * 24 / 375).dp,
+                    end = (width * 24 / 375).dp
+                )
+                .fillMaxSize()
+        ) {
+            val (text, lazy, btn) = createRefs()
+            Text(
+                "What is your Mother language?",
+                fontFamily = fonts,
+                fontWeight = FontWeight.Medium,
+                fontSize = 22.sp,
+                color = Color(0xFF080E1E),
+                modifier = Modifier
+                    .constrainAs(text) {
+                        top.linkTo(parent.top)
+                        start.linkTo(parent.start)
+                    }
+                    .padding(bottom = (height * 16 / 812).dp)
+
+            )
+            val ind = remember { mutableStateOf("Russian") }
+            LazyColumn(
+                verticalArrangement = Arrangement.spacedBy(
+                    (height * 12 / 812).dp,
+                    Alignment.CenterVertically
+                ),
+                modifier = Modifier.constrainAs(lazy) {
+                    top.linkTo(text.bottom)
+                    start.linkTo(parent.start)
+                }
+            ) {
+                itemsIndexed(languages) { index, lang ->
+                    Button(
+                        onClick = {
+                            ind.value = lang
+                        },
+                        modifier = Modifier
+                            .fillMaxWidth(),
+                        colors = ButtonDefaults.buttonColors(
+                            containerColor =
+                                if (ind.value == lang) {
+                                    Color(0xFFF76400)
+                                } else {
+                                    Color(0xFFFFF6EB)
+                                }
+                        ),
+                        shape = RoundedCornerShape(20.dp)
+                    ) {
+                        Text(
+                            lang,
+                            modifier = Modifier
+                                .padding(
+                                    top = (height * 20 / 812).dp,
+                                    bottom = (height * 20 / 812).dp
+                                )
+                                .fillMaxWidth(),
+                            fontFamily = fonts,
+                            fontWeight = FontWeight.Medium,
+                            fontSize = (height * 22 / 812).sp,
+                            color = Color.Black,
+                            textAlign = TextAlign.Start
+                        )
+                    }
+
+                }
+            }
+            Button(
+                onClick = { navController.navigate(Route.SignUp.route) },
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .constrainAs(btn) {
+                        bottom.linkTo(parent.bottom, margin = (height * 25 / 812).dp)
+                        start.linkTo(parent.start)
+                    },
+                colors = ButtonDefaults.buttonColors(
+                    Color(0xFF5B7BFE)
+                ),
+                shape = RoundedCornerShape(16.dp)
+            ) {
+                Text(
+                    "Choose",
+                    fontFamily = fonts,
+                    fontWeight = FontWeight.Medium,
+                    fontSize = (height * 20 / 812).sp,
+                    color = Color.White,
+                    modifier = Modifier.padding(vertical = (height * 16 / 812).dp)
+                )
+            }
+
+        }
     }
 }
--- app/src/main/java/com/example/test/sign_up/SignUpScreen.kt
+++ app/src/main/java/com/example/test/sign_up/SignUpScreen.kt
@@ -1,10 +1,313 @@
 package com.example.test.sign_up
 
+import android.util.Log
+import android.widget.Toast
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.AlertDialog
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.Text
+import androidx.compose.material3.TextButton
+import androidx.compose.material3.TextField
+import androidx.compose.material3.TextFieldDefaults
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.vector.ImageVector
+import androidx.compose.ui.platform.LocalConfiguration
+import androidx.compose.ui.res.vectorResource
+import androidx.compose.ui.text.font.Font
+import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import androidx.navigation.NavController
+import com.example.test.R
+import com.example.test.Route
 
+
+fun isValid(value: String): Boolean {
+
+    try {
+        val name = value.substring(0, value.indexOf("@"))
+        val domen = value.substring(value.indexOf("@") + 1, value.indexOf("."))
+        val flag = value.indexOf("@") < value.indexOf(".")
+        val flag2 = name.lowercase() == name
+        val flag3 = domen.lowercase() == domen
+
+        if (!value.contains("@") or !value.contains(".") or !flag or !flag2 or !flag3)
+            return false
+        else return true
+    } catch (e: Exception) {
+        return false
+    }
+}
+
+/*
+    var showDialog by remember { mutableStateOf(false) }
+
+    // Кнопка, которая открывает диалог
+    Button(onClick = { showDialog = true }) {
+        Text("Показать сообщение")
+    }
+
+    // Вызов диалога
+    MyDialog(
+        title = "Важное сообщение",
+        text = "Это пример окна сообщения в Jetpack Compose.",
+        show = showDialog,
+        onDismissRequest = { showDialog = false }, // Закрытие по клику вне окна
+        confirmButton = {
+            TextButton(onClick = { showDialog = false }) {
+                Text("OK")
+            }
+        }
+    )
+}
+*/
+@Composable
+fun MyDialog(
+    title: String,
+    text: String,
+    show: Boolean,
+    onDismissRequest: () -> Unit,
+    confirmButton: @Composable () -> Unit
+) {
+    if (show) {
+        AlertDialog(
+            onDismissRequest = onDismissRequest,
+            title = { Text(title) },
+            text = { Text(text) },
+            confirmButton = { confirmButton() }
+        )
+    }
+}
 
 @Composable
 fun SignUpScreen(
     navController: NavController
-) {}
+) {
+    var showDialog = remember { mutableStateOf(false) }
+
+    val fonts = FontFamily(
+        Font(R.font.fredoka_m, FontWeight.Medium), Font(R.font.fredoka_r, FontWeight.Normal)
+    )
+    val configuration = LocalConfiguration.current
+    val width = configuration.screenWidthDp
+    val height = configuration.screenHeightDp
+
+    Column() {
+        Box(
+            modifier = Modifier
+                .fillMaxWidth()
+                .background(Color(0xFF410FA3))
+        ) {
+            Image(
+                imageVector = ImageVector.vectorResource(R.drawable.icon_back),
+                "back",
+                modifier = Modifier
+                    .padding(
+                        top = (height * 61 / 812).dp,
+                        start = (width * 24 / 375).dp,
+                        bottom = (height * 21 / 812).dp
+                    )
+                    .clickable { navController.navigate(Route.LanguageSelect.route) })
+
+            Text(
+                "Sign up",
+                fontFamily = fonts,
+                fontWeight = FontWeight.Medium,
+                fontSize = (height * 17 / 812).sp,
+                color = Color.White,
+                modifier = Modifier
+                    .padding(
+                        top = (height * 60 / 812).dp, bottom = (height * 20 / 812).dp
+                    )
+                    .align(Alignment.Center)
+            )
+
+        }
+
+
+        Column(
+            modifier = Modifier
+                .padding(
+                    top = (height * 40 / 812).dp,
+                    start = (width * 24 / 375).dp,
+                    end = (width * 24 / 375).dp
+                )
+                .fillMaxSize()
+        ) {
+            val first_name = remember { mutableStateOf("") }
+            val last_name = remember { mutableStateOf("") }
+            val email = remember { mutableStateOf("") }
+            Text(
+                "Create an Account",
+                modifier = Modifier
+                    .align(Alignment.CenterHorizontally)
+                    .padding(bottom = (height * 8 / 812).dp),
+                fontFamily = fonts,
+                fontWeight = FontWeight.Medium,
+                fontSize = 22.sp,
+                color = Color(0xFF080E1E)
+            )
+
+            Text(
+                "First name",
+                fontFamily = fonts,
+                fontWeight = FontWeight.Normal,
+                fontSize = (height * 15 / 812).sp,
+                color = Color(0xFF363B44),
+                modifier = Modifier.padding(
+                    top = (height * 24 / 812).dp, bottom = (height * 8 / 812).dp
+                )
+            )
+            TextField(
+                value = first_name.value,
+                onValueChange = { first_name.value = it },
+                modifier = Modifier.fillMaxWidth(),
+                colors = TextFieldDefaults.colors(
+                    unfocusedContainerColor = Color(0x05080E1E),
+                    unfocusedIndicatorColor = Color.Transparent,
+                    focusedIndicatorColor = Color.Transparent
+                ),
+                shape = RoundedCornerShape(16.dp),
+                placeholder = {
+                    Text(
+                        "Your first name",
+                        fontFamily = fonts,
+                        fontWeight = FontWeight.Normal,
+                        fontSize = (height * 15 / 812).sp,
+                        color = Color(0x50656872),
+
+
+                        )
+                })
+            Text(
+                "Last name",
+                fontFamily = fonts,
+                fontWeight = FontWeight.Normal,
+                fontSize = (height * 15 / 812).sp,
+                color = Color(0xFF363B44),
+                modifier = Modifier.padding(
+                    top = (height * 24 / 812).dp, bottom = (height * 8 / 812).dp
+                )
+            )
+            TextField(
+                value = last_name.value,
+                onValueChange = { last_name.value = it },
+                modifier = Modifier.fillMaxWidth(),
+                colors = TextFieldDefaults.colors(
+                    unfocusedContainerColor = Color(0x05080E1E),
+                    unfocusedIndicatorColor = Color.Transparent,
+                    focusedIndicatorColor = Color.Transparent
+                ),
+                shape = RoundedCornerShape(16.dp),
+                placeholder = {
+                    Text(
+                        "Your last name",
+                        fontFamily = fonts,
+                        fontWeight = FontWeight.Normal,
+                        fontSize = (height * 15 / 812).sp,
+                        color = Color(0x50656872),
+
+
+                        )
+                })
+            Text(
+                "Email",
+                fontFamily = fonts,
+                fontWeight = FontWeight.Normal,
+                fontSize = (height * 15 / 812).sp,
+                color = Color(0xFF363B44),
+                modifier = Modifier.padding(
+                    top = (height * 24 / 812).dp, bottom = (height * 8 / 812).dp
+                )
+            )
+            TextField(
+                value = email.value,
+                onValueChange = { email.value = it },
+                modifier = Modifier.fillMaxWidth(),
+                colors = TextFieldDefaults.colors(
+                    unfocusedContainerColor = Color(0x05080E1E),
+                    unfocusedIndicatorColor = Color.Transparent,
+                    focusedIndicatorColor = Color.Transparent
+                ),
+                shape = RoundedCornerShape(16.dp),
+                placeholder = {
+                    Text(
+                        "Email",
+                        fontFamily = fonts,
+                        fontWeight = FontWeight.Normal,
+                        fontSize = (height * 15 / 812).sp,
+                        color = Color(0x50656872),
+                    )
+                }
+            )
+            Button(
+                onClick = {
+                    if (isValid(email.value)) navController.navigate(Route.LogIn.route)
+                    else showDialog.value = true
+                },
+                modifier = Modifier
+                    .align(Alignment.CenterHorizontally)
+                    .fillMaxWidth()
+                    .padding(top = (height * 34 / 812).dp, bottom = (height * 24 / 812).dp),
+                colors = ButtonDefaults.buttonColors(
+                    Color(0xFF5B7BFE)
+                ),
+                shape = RoundedCornerShape(12.dp)
+            ) {
+                Text(
+                    "Continue",
+                    fontFamily = fonts,
+                    fontWeight = FontWeight.Medium,
+                    fontSize = 20.sp,
+                    color = Color.White,
+                    modifier = Modifier.padding(vertical = (height * 16 / 812).dp)
+                )
+            }
+            Row(modifier = Modifier.align(Alignment.CenterHorizontally)) {
+                Text(
+                    "Already you member? ",
+                    fontFamily = fonts,
+                    fontWeight = FontWeight.Normal,
+                    fontSize = (height * 17 / 812).sp,
+                    color = Color(0xFF656872),
+                )
+                Text(
+                    "Login",
+                    fontFamily = fonts,
+                    fontWeight = FontWeight.Medium,
+                    fontSize = (height * 17 / 812).sp,
+                    color = Color(0xFF5B7BFE),
+                    modifier = Modifier.clickable { navController.navigate(Route.SignUpPassword.route) })
+
+            }
+            MyDialog(
+                title = "Ошибка",
+                text = "Введите корректный email",
+                show = showDialog.value,
+                onDismissRequest = { showDialog.value = false }, // Закрытие по клику вне окна
+                confirmButton = {
+                    TextButton(onClick = { showDialog.value = false
+                    email.value = ""}) {
+                        Text("OK")
+                    }
+                }
+            )
+        }
+    }
+}
